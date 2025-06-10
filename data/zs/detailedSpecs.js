030fb0ae470c10af24013a4b76ea319342f446a8
Harry-0824 <prince11290824@gmail.com>
2025-06-10 17:52:44 +0800

    建立規格手風琴元件

---
+++ data/zs/detailedSpecs.js
@@ -0,0 +1,49 @@
+export const zsDetailedSpecs = {
+  "ZS 1.5L 旗艦版": {
+    基本規格: [
+      { label: "全長x全寬x全高 (mm)", value: "4,323×1,809×1,653" },
+      { label: "軸距 (mm)", value: "2585" },
+      { label: "輪胎尺寸", value: "215/55/R17" },
+      { label: "車重(Kg)", value: "1,318" },
+      { label: "油箱容量(L)", value: "45" },
+      { label: "煞車系統", value: "碟煞" },
+      { label: "轉向系統", value: "EPS電動輔助轉向系統(三段輔助力道調整)" },
+      { label: "懸吊系統 (前/後)", value: "麥花臣式附防傾桿/扭力樑式" },
+    ],
+    // 預留其他規格分類，您可以後續填寫
+    動力系統: [
+      { label: "引擎型式", value: "1.5L DVVT雙連續可變汽門正時汽油引擎" },
+      { label: "排氣量", value: "1,498" },
+      { label: "驅動方式", value: "前輪驅動" },
+      { label: "最大馬力 (ps/rpm)", value: "120/6,000" },
+      { label: "最大扭力 (kg-m/rpm)", value: "15.3/4,500" },
+      { label: "變速箱型式", value: "CVT無段變速系統(附模擬8速手自排)" },
+    ],
+    外觀配置: [
+      { label: "黑曜矩陣水箱護罩", value: "●" },
+      { label: "銀石之翼LED頭燈", value: "●" },
+      { label: "轉向輔助照明功能", value: "●" },
+      { label: "LED日行燈", value: "●" },
+      { label: "光羽LED尾燈", value: "●" },
+      { label: "前霧燈", value: "●" },
+      { label: "LED 前方向燈/LED 後方向燈", value: "●" },
+      { label: "光感應頭尾燈自動啟閉系統", value: "●" },
+      { label: "電動收折後視鏡附方向指示燈", value: "●" },
+      { label: "沉浸式全景天窗(附電動遮陽簾及防夾功能)", value: "●" },
+      { label: "霧銀車頂行李架", value: "●" },
+      { label: "酷黑鯊魚鰭天線", value: "●" },
+      { label: "17吋銀黑切削鋁圈", value: "●" },
+      { label: "鍍鉻窗框飾條/車門把手", value: "●" },
+      { label: "霧銀風格飾件(車身下緣)", value: "●" },
+    ],
+    座艙配置: [],
+    座椅配置: [],
+    "音響/娛樂系統": [],
+    "MG PILOT 2.0 Level 2 智慧駕駛輔助": [],
+    安全科技: [],
+    隨車配件: [],
+    能源效率: [],
+  },
+  // 如果有其他 ZS 車型，可以繼續在此添加
+  // "ZS 1.5L 豪華版": { ... }
+};
